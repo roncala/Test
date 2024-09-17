79c9425ec62748f5fa1516ad6505cc71cf69941b
roncala <roncala@kean.edu>
2024-09-17 11:52:03 -0400

    Add files via upload

---
+++ dicitionary.py
@@ -0,0 +1,16 @@
+#list
+
+#acronym = ['lol, idk, tbh']
+
+# dicitionary maps keys to values
+
+acronyms = {
+    'lol': 'laught out loud',
+    'idk': 'i dont know',
+    'tbh': 'to be honest'
+}
+
+
+print(acronyms['tbh'])
+
+#use del acronyms['lol']
